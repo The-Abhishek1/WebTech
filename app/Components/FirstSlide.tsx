c51754da563ca4e93c3b7d95431d4da3e599d2e7
The-Abhishek1 <idiot63666@gmail.com>
2024-03-25 22:21:11 +0530

    Made the pages responsive

--- app/Components/FirstSlide.tsx
+++ app/Components/FirstSlide.tsx
@@ -7,7 +7,7 @@ import image from "@/publicresponsive-design.png";
 export default function FirstSlide() {
   return (
     <div className="bg-cyan-50 flex flex-row items-center justify-around p-4 py-8 msm:flex-col-reverse gap-4">
-      <div className="flex flex-col items-center justify-around smm:ml-10 gap-4 ">
+      <div className="flex flex-col w-[450px] items-center justify-around smm:ml-10 gap-4 msm:w-[350px] msmm:w-[250px]">
         <h2 className="text-[40px] font-bold transition ease-in-out duration-700 delay-700 mmd:text-[25px]">
           Welcome
         </h2>
@@ -16,7 +16,7 @@ export default function FirstSlide() {
           UI/UX design. Let us ignite your digital journey! 💻📱
         </p>
         <q className="italic">#InnovationUnleashed</q>
-        <div className="p-2 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[150px]">
+        <div className="p-2 cursor-pointer rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[150px]">
           <button className="">About Us</button>
           <FaArrowRightLong />
         </div>
@@ -26,7 +26,8 @@ export default function FirstSlide() {
           src={image}
           priority={true}
           alt="Start Image"
-          className="aspect-square[2/1] max-h-[450px] msm:max-h-[250px] msm:max-w-[250px] msmm:max-h-[200px] msmm:max-w[150px]"
+          className="aspect-square[2/1] max-h-[450px] msm:max-h-[250px] msm:max-w-[250px] msmm:max-h-[200px] msmm:max-w[150px] mxl:w-[400px]
+          transition ease-in-out delay-200 cursor-progress hover:-translate-y-1 hover:scale-95 duration-500"
         />
       </div>
     </div>
--- app/Components/SecondSlide.jsx
+++ app/Components/SecondSlide.jsx
@@ -2,49 +2,63 @@ import Image from "next/image";
 import React from "react";
 import { BsMotherboard } from "react-icons/bs";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { ImCheckboxChecked } from "react-icons/im";
 import photo1 from "@/publicdesign.png";
 import photo2 from "@/publicapi.png";
 import photo3 from "@/publicweb-maintenance.png";
 import { auto } from "openai/_shims/registry.mjs";
+
 //Main Function
 export default function SecondSlide() {
   return (
-    <div className="relative w-full flex flex-row gap-4 justify-around p-10">
-      <div className="relative">
+    <div className="relative bg-cyan-50 border-t-[1px] border-gray-200 w-full flex lg:flex-row flex-col items-center gap-4 justify-around p-4 py-8">
+      <div className="relative top-[40px]">
         <Image
           alt="First"
           height={auto}
           src={photo1}
-          className="animate-bounce"
+          className="animate-bounce cursor-progress msm:max-h-[250px] msm:max-w-[250px] msmm:max-h-[200px] msmm:max-w[120px] mxl:w-[400px] msmm:mt-[-20px]"
         />
       </div>
-      <div></div>
-      <div className="flex flex-col w-[400px]">
-        <div>
-          <BsMotherboard />
-          <h3>About WebTech IT Solutions</h3>
+      <div className="flex flex-col sm:w-[500px] msm:px-3 gap-5">
+        <div className="flex flex-row items-center justify-center gap-3  text-blue-600">
+          <BsMotherboard size={30} />
+          <h3 className="text-[18px] mmd:text-[15px]">
+            About WebTech IT Solutions
+          </h3>
         </div>
-        <div>Preparing for your business provide best IT Solution</div>
-        <div className="">
-          Quickly re-engineer intuitive e-services whereas complelling.
-          Professionally syndicate startegic e-commerce without covalent
-          leadership skills. Globally customize equity invested imperatives.
+        <div className="text-[30px] mmd:text-[20px] font-bold text-center">
+          Preparing for your business provide best
+          <p className="inline text-blue-600"> IT Solution</p>
         </div>
         <div>
-          <div>
-            <input type="checkbox" name="checked" checked disabled />
-            <p>Dramatically re-engineer value added IT systems via mission</p>
+          <p className="text-center text-[14px] mmd:text-[13px]">
+            Quickly re-engineer intuitive e-services whereas complelling.
+            Professionally syndicate startegic e-commerce without covalent
+            leadership skills. Globally customize equity invested imperatives.
+          </p>
+        </div>
+        <div className="flex flex-col justify-between gap-5">
+          <div className="flex flex-row items-center gap-2">
+            <ImCheckboxChecked size={20} color="blue" />
+            <p className="text-[14px] mmd:text-[12px]">
+              Dramatically re-engineer value added IT systems via mission
+            </p>
           </div>
-          <div>
-            <input type="checkbox" name="checked" checked disabled />
-            <p>Website & Mobile application design & Development</p>
+          <div className="flex flex-row items-center gap-2">
+            <ImCheckboxChecked size={20} color="blue" />
+            <p className="text-[14px] mmd:text-[12px]">
+              Website & Mobile application design & Development
+            </p>
           </div>
-          <div>
-            <input type="checkbox" name="checked" checked disabled />
-            <p>professional User Experience & Interface researching</p>
+          <div className="flex flex-row items-center gap-2">
+            <ImCheckboxChecked size={20} color="blue" />
+            <p className="text-[14px] mmd:text-[12px]">
+              professional User Experience & Interface researching
+            </p>
           </div>
-          <div>
-            <button>Discover More</button>
+          <div className="p-2 cursor-pointer rounded-md flex flex-row items-center self-center justify-center gap-2 bg-blue-500 text-white w-[150px]">
+            <button className="">Discover More</button>
             <FaLongArrowAltRight />
           </div>
         </div>
