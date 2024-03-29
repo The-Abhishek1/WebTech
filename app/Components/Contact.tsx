522cebae36b8943a2579453c7ef29b9e361e1ca8
The-Abhishek1 <idiot63666@gmail.com>
2024-03-29 10:00:58 +0530

    Added Hover Effect

--- app/Components/Contact.tsx
+++ app/Components/Contact.tsx
@@ -13,7 +13,7 @@ export default function Contact() {
         <h1 className="text-center">Need Any kind Of IT Solution For</h1>
         <h1 className="text-center text-blue-600 font-normal">Your Business</h1>
       </div>
-      <div className="p-2 cursor-pointer rounded-md flex flex-row shadow-md shadow-blue-400 items-center self-center justify-center gap-2 bg-blue-500 text-white w-[170px]">
+      <div className="p-2 hover:bg-indigo-950 hover:shadow-slate-100 hover:shadow-sm cursor-pointer rounded-md flex flex-row shadow-md shadow-blue-400 items-center self-center justify-center gap-2 bg-blue-500 text-white w-[170px]">
         <button className="uppercase font-bold text-[15px]">
           Get in Touch
         </button>
--- app/Components/Package.tsx
+++ app/Components/Package.tsx
@@ -20,8 +20,8 @@ export default function Package() {
         </div>
       </div>
       <div className="flex flex-row flex-wrap justify-around gap-10">
-        <div className="flex flex-col gap-3 w-[280px] bg-slate-200 rounded-lg">
-          <div className="flex flex-col gap-3 bg-indigo-950 text-white p-5 rounded-lg">
+        <div className="flex flex-col gap-3 w-[280px] bg-slate-200 rounded-lg group">
+          <div className="flex flex-col gap-3 bg-indigo-950 group-hover:bg-blue-500 text-white p-5 rounded-lg">
             <div className="flex flex-col gap-3">
               <h1 className="font-bold text-[20px] mmd:text-[16px]">
                 Silver Package
@@ -68,7 +68,7 @@ export default function Package() {
                 </p>
               </div>
             </div>
-            <div className="p-2 self-center cursor-pointer shadow-lg shadow-indigo-950 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[180px]">
+            <div className="p-2 self-center cursor-pointer shadow-lg shadow-indigo-950 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[180px] hover:bg-indigo-950">
               <button className="uppercase font-bold text-[15px] mmd:text-[13px]">
                 Purchase Now
               </button>
@@ -76,8 +76,8 @@ export default function Package() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-3 w-[280px] bg-slate-200 rounded-lg">
-          <div className="flex flex-col gap-3 bg-indigo-950 text-white p-5 rounded-lg">
+        <div className="flex flex-col gap-3 w-[280px] bg-slate-200 rounded-lg group">
+          <div className="flex flex-col gap-3 bg-indigo-950 group-hover:bg-blue-500 text-white p-5 rounded-lg">
             <div className="flex flex-col gap-3">
               <h1 className="font-bold text-[20px] mmd:text-[16px]">
                 Gold Package
@@ -124,7 +124,7 @@ export default function Package() {
                 </p>
               </div>
             </div>
-            <div className="p-2 self-center cursor-pointer shadow-lg shadow-indigo-950 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[180px]">
+            <div className="p-2 self-center cursor-pointer shadow-lg shadow-indigo-950 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[180px] hover:bg-indigo-950">
               <button className="uppercase font-bold text-[15px] mmd:text-[13px]">
                 Purchase Now
               </button>
@@ -132,8 +132,8 @@ export default function Package() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-3 w-[280px] bg-slate-200 rounded-lg">
-          <div className="flex flex-col gap-3 bg-indigo-950 text-white p-5 rounded-lg">
+        <div className="flex flex-col gap-3 w-[280px] bg-slate-200 rounded-lg group">
+          <div className="flex flex-col gap-3 bg-indigo-950 group-hover:bg-blue-500 text-white p-5 rounded-lg">
             <div className="flex flex-col gap-3">
               <h1 className="font-bold text-[20px] mmd:text-[16px]">
                 Platinum Package
@@ -180,7 +180,7 @@ export default function Package() {
                 </p>
               </div>
             </div>
-            <div className="p-2 self-center cursor-pointer shadow-lg shadow-indigo-950 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[180px]">
+            <div className="p-2 self-center cursor-pointer shadow-lg shadow-indigo-950 rounded-md flex flex-row items-center justify-center gap-2 bg-blue-500 text-white w-[180px] hover:bg-indigo-950">
               <button className="uppercase font-bold text-[15px] mmd:text-[13px]">
                 Purchase Now
               </button>
