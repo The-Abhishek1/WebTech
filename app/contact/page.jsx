0da926b001615c9ccfd3c5c7a549077383d88564
The-Abhishek1 <idiot63666@gmail.com>
2024-04-19 23:15:30 +0530

    Enhanced Contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Footer from "../Components/Footer";
-import ContactFirstSlide from "./(contactComponents)/contactFirstSlide";
+import ContactFirstSlide from "./(contactComponents)/ContactFirstSlide";
+import ContactLinks from "./(contactComponents)/ContactLinks";
+import MessagePage from "./(contactComponents)/MessagePage";
 
+//Main Function
 export default function page() {
   return (
     <>
       <ContactFirstSlide />
+      <ContactLinks />
+      <MessagePage />
       <Footer />
     </>
   );
